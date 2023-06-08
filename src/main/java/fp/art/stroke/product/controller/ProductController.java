580db85fdd7d3b6d26ad94c391446b3e296fdf72
sharon <jhj.sharon@gmail.com>
2023-06-08 21:10:57 +0900

    sharon/thumbnail/0608

--- src/main/java/fp/art/stroke/product/controller/ProductController.java
+++ src/main/java/fp/art/stroke/product/controller/ProductController.java
@@ -2,11 +2,17 @@ package fp.art.stroke.product.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import fp.art.stroke.product.model.service.ProductService;
+import fp.art.stroke.product.model.vo.Product;
+
+import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,10 +27,11 @@ public class ProductController {
 	
 	private Logger logger = LoggerFactory.getLogger(ProductController.class);
 	
-	   @GetMapping("/productMain")
-	   public String productMain() {
-	      return "product/productMain";
-	   }
+
+//	   @GetMapping("/productMain")
+//	   public String productMain() {
+//	      return "product/productMain";
+//	   }
 	   @GetMapping("/productDetail")
 	   public String prouctDetail() {
 	      return "product/productDetail";
@@ -66,6 +73,29 @@ public class ProductController {
 		   
 		   return"product/productSearch";
 	   }
+	   
+	   
+		//상품 메인페이지 : 상품목록
+		@GetMapping("/productMain")
+		public String loadproductList(
+				@RequestParam(value = "cp", required = false, defaultValue = "1" )  int cp
+				,Model model
+				,@RequestParam Map<String, Object> paramMap){
+			
+			Map<String, Object> map = null;
+			
+			
+			
+		    //List<Product> productList = service.loadProductList(cp);
+			map=service.loadProductList(cp);
+			
+		    model.addAttribute("map", map);
+
+		    return "product/productMain"; // 상품 목록을 보여주는 뷰의 이름을 반환
+			
+		}
+		
+		
 	   
 
 
--- src/main/java/fp/art/stroke/product/model/dao/ProductDAO.java
+++ src/main/java/fp/art/stroke/product/model/dao/ProductDAO.java
@@ -2,10 +2,12 @@ package fp.art.stroke.product.model.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import fp.art.stroke.product.model.vo.Pagination;
 import fp.art.stroke.product.model.vo.Product;
 
 @Repository 
@@ -17,5 +19,28 @@ public class ProductDAO {
 	public List<Product> getWriterProductList(int memberId) {
 		// TODO Auto-generated method stub
 		return sqlSession.selectList("productMapper.getWriterProductList",memberId);
+	}
+
+	/**전체 상품 수 조회 DAO
+	 * @return
+	 */
+	public int getListCount() {
+		
+		return sqlSession.selectOne("productMapper.getListCountP");
+	}
+
+	/**상품 목록 조회 DAO
+	 * @param pagination
+	 * @return
+	 */
+	public List<Product> loadProductList(Pagination pagination) {
+		 
+		//현재 페이지에 해당하는 만큼의 상품만 가져오기
+		
+		int offset = (pagination.getCurrentPage() - 1 ) *pagination.getLimit();
+		
+		RowBounds rowBounds = new RowBounds(offset, pagination.getLimit());
+		
+		return sqlSession.selectList("productMapper.loadProductList", rowBounds);
 	}
 }
